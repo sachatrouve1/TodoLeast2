7a947cda59cda226e6669d0e7e15e7be1f2aa1f7
Julian <rayconstantyjulian@gmail.com>
2026-03-08 19:13:50 +0100

    Add color palette

---
+++ app/src/main/java/com/app/todoleast/ui/theme/Color.kt
@@ -0,0 +1,64 @@
+package com.app.todoleast.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary colors - Deep blue-purple gradient feel
+val PrimaryLight = Color(0xFF6750A4)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFFE9DDFF)
+val OnPrimaryContainerLight = Color(0xFF22005D)
+
+val PrimaryDark = Color(0xFFCFBCFF)
+val OnPrimaryDark = Color(0xFF381E72)
+val PrimaryContainerDark = Color(0xFF4F378A)
+val OnPrimaryContainerDark = Color(0xFFE9DDFF)
+
+// Secondary colors
+val SecondaryLight = Color(0xFF625B71)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFE8DEF8)
+val OnSecondaryContainerLight = Color(0xFF1E192B)
+
+val SecondaryDark = Color(0xFFCBC2DB)
+val OnSecondaryDark = Color(0xFF332D41)
+val SecondaryContainerDark = Color(0xFF4A4458)
+val OnSecondaryContainerDark = Color(0xFFE8DEF8)
+
+// Tertiary colors - for accents
+val TertiaryLight = Color(0xFF7E5260)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFFFD9E3)
+val OnTertiaryContainerLight = Color(0xFF31101D)
+
+val TertiaryDark = Color(0xFFEFB8C8)
+val OnTertiaryDark = Color(0xFF4A2532)
+val TertiaryContainerDark = Color(0xFF633B48)
+val OnTertiaryContainerDark = Color(0xFFFFD9E3)
+
+// Background and surface
+val BackgroundLight = Color(0xFFFFFBFF)
+val OnBackgroundLight = Color(0xFF1C1B1E)
+val SurfaceLight = Color(0xFFFFFBFF)
+val OnSurfaceLight = Color(0xFF1C1B1E)
+
+val BackgroundDark = Color(0xFF1C1B1E)
+val OnBackgroundDark = Color(0xFFE6E1E6)
+val SurfaceDark = Color(0xFF1C1B1E)
+val OnSurfaceDark = Color(0xFFE6E1E6)
+
+// Status colors
+val StatusToDo = Color(0xFF6750A4)
+val StatusOnGoing = Color(0xFFFF9800)
+val StatusCompleted = Color(0xFF4CAF50)
+val StatusOverdue = Color(0xFFE53935)
+
+// Error
+val ErrorLight = Color(0xFFBA1A1A)
+val OnErrorLight = Color(0xFFFFFFFF)
+val ErrorContainerLight = Color(0xFFFFDAD6)
+val OnErrorContainerLight = Color(0xFF410002)
+
+val ErrorDark = Color(0xFFFFB4AB)
+val OnErrorDark = Color(0xFF690005)
+val ErrorContainerDark = Color(0xFF93000A)
+val OnErrorContainerDark = Color(0xFFFFDAD6)
