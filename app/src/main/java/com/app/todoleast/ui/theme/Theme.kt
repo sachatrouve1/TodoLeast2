bf2b31dc26b2ff78a5290e1ebb6744b821a80fd3
Julian <rayconstantyjulian@gmail.com>
2026-03-08 19:35:24 +0100

    Remove deprecated statusBarColor

--- app/src/main/java/com/app/todoleast/ui/theme/Theme.kt
+++ app/src/main/java/com/app/todoleast/ui/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -68,7 +67,6 @@ fun TodoLeastTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
